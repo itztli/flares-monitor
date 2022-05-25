5924ece4a5bc65b2583d1977d0f5c03fda97f516
Gilberto Dominguez <gilbertocarlos.dominguez@gmail.com>
2022-05-25 10:44:30 -0500

    processing

--- 02_processing/02_processing.py
+++ 02_processing/02_processing.py
@@ -3,6 +3,9 @@ import psycopg2
 from config import config
 import xml.etree.ElementTree as ET
 from datetime import datetime
+import matplotlib.pyplot as plt
+
+swpc_to_watts = {"X":1e-4,"M":1e-5,"C":1e-6,"B":1e-7,"A":1e-8} #W/m^2
 
 def connect():
     """ Connect to the PostgreSQL database server """
@@ -23,17 +26,32 @@ def connect():
         #cur.execute('SELECT version()')
 
         sql = "SELECT * from flares"
-
         cur.execute(sql)
-
         records = cur.fetchall()
+        cur.close()
 
+        X =[]
+        Y= []
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        #ax.set_aspect('equal')
+
+        
         for row in records:
+            X.append(datetime.strptime(row[0], '%Y-%m-%d %H:%M:%S'))
+            flare_class = row[1]
+            event = flare_class.split('.')
+            watts = float(event[1])*swpc_to_watts[event[0]]
+            Y.append(watts)
             print("date = ", row[0], )
             print("class = ", row[1])
+
+        # close the communication with the PostgreSQL
         
-	# close the communication with the PostgreSQL
-        cur.close()
+        ax.plot(X,Y)
+
+        plt.savefig("flares.png")
+        
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
     finally:
