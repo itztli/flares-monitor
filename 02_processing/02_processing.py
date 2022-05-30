944dbf581382481c76f561f1e361aee04c6bca22
Gilberto Dominguez <gilbertocarlos.dominguez@gmail.com>
2022-05-30 10:33:47 -0500

    processing

--- 02_processing/02_processing.py
+++ 02_processing/02_processing.py
@@ -4,6 +4,8 @@ from config import config
 import xml.etree.ElementTree as ET
 from datetime import datetime
 import matplotlib.pyplot as plt
+import subprocess
+
 
 swpc_to_watts = {"X":1e-4,"M":1e-5,"C":1e-6,"B":1e-7,"A":1e-8} #W/m^2
 
@@ -51,6 +53,12 @@ def connect():
         ax.plot(X,Y)
 
         plt.savefig("/data/vdelaluz/flares.png")
+
+        process = subprocess.Popen(['scp /data/vdelaluz/flares.png vdelaluz@10.99.1.138:/home/vdelaluz/public_html/gicc/static/flares/'],
+                                   stdout=subprocess.PIPE, 
+                                   stderr=subprocess.PIPE)        
+        stdout, stderr = process.communicate()
+        print(stdout, stderr)
         
     except (Exception, psycopg2.DatabaseError) as error:
         print(error)
